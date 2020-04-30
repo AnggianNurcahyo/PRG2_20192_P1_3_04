1337f0f162666520fce3318dcacebf1fa4d2690f
Anggian Nurcahyo <anggian.nurcahyo11@gmail.com>
2020-04-30 13:29:56 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PRG2_20192_P1_3_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan banyaknya N : ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            while(n > 1)
+            {
+                Console.WriteLine("Anak ayam turun {0}, mati satu tinggal {1}", n, n-1);
+                n--;
+            }
+            Console.WriteLine("Anak ayam turun {0}, mati satu tinggal induknya",n);
+        }
+    }
+}
